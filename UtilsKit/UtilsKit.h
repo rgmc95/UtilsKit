8b18d2d5b799990c49da3c3a2030898a03fefa8c
Michael Coqueret <michael.coqueret@id-apps.fr>
2019-12-23 13:33:45 +0100

    Initial Commit

---
+++ UtilsKit/UtilsKit.h
@@ -0,0 +1,19 @@
+//
+//  UtilsKit.h
+//  UtilsKit
+//
+//  Created by Michael Coqueret on 23/12/2019.
+//  Copyright © 2019 iD.apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UtilsKit.
+FOUNDATION_EXPORT double UtilsKitVersionNumber;
+
+//! Project version string for UtilsKit.
+FOUNDATION_EXPORT const unsigned char UtilsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilsKit/PublicHeader.h>
+
+
