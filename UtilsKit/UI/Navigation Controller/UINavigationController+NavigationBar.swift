869e20633410930f81b49c72ff75316fc2289daf
Michael Coqueret <mcoqueret@exomind.fr>
2021-03-25 16:03:42 +0100

    [Navigation Bar] add argument (on/off) in setNavigationBarTransparent

--- UtilsKit/UI/Navigation Controller/UINavigationController+NavigationBar.swift
+++ UtilsKit/UI/Navigation Controller/UINavigationController+NavigationBar.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 extension UINavigationController {
-    
-    /**
-        Clear the navigation bar background, navigation bar is present but transparent
-     */
-    public func setNavigationBarTransparent() {
-        self.navigationBar.removeShadow()
-        self.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        self.isNavigationBarHidden = false
-        self.navigationBar.isTranslucent = true
-    }
+	
+	/**
+	Clear the navigation bar background, navigation bar is present but transparent
+	*/
+	public func setNavigationBarTransparent(_ value: Bool) {
+		if value {
+			self.navigationBar.removeShadow()
+			self.navigationBar.isTranslucent = true
+			self.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+		} else {
+			self.navigationBar.addShadow()
+			self.navigationBar.isTranslucent = false
+			self.navigationBar.setBackgroundImage(nil, for: UIBarMetrics.default)
+		}
+		
+		self.isNavigationBarHidden = false
+	}
 }
