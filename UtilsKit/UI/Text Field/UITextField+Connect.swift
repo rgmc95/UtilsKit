346d883bd1a39651dfce209dd3f23b616cf45359
Michael Coqueret <mcoqueret@exomind.fr>
2021-09-02 17:05:26 +0200

    [TextField] connect next button

---
+++ UtilsKit/UI/Text Field/UITextField+Connect.swift
@@ -0,0 +1,26 @@
+//
+//  UITextField+Connect.swift
+//  UtilsKit
+//
+//  Created by Michael Coqueret on 01/09/2021.
+//  Copyright © 2021 iD.apps. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+	
+	/// Connect next textfield button
+	public static func connectTextFields(_ textFields: UITextField...,
+										 withFinalReturnKey returnKeyType: UIReturnKeyType = .go) {
+		if let last = textFields.last { last.returnKeyType = returnKeyType }
+		
+		for item in 0 ..< textFields.count - 1 {
+			textFields[item].returnKeyType = .next
+			textFields[item].addTarget(
+				textFields[item + 1],
+				action: #selector(UIResponder.becomeFirstResponder),
+				for: .editingDidEndOnExit)
+		}
+	}
+}
