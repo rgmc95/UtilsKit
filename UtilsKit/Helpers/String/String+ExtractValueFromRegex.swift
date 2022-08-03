262f5c3bd5e05e06b85bcdcc25920fdf4041eceb
Michael Coqueret <mcoqueret@exomind.fr>
2022-08-03 19:32:16 +0200

    [Regex] fix values with regex

--- UtilsKit/Helpers/String/String+ExtractValueFromRegex.swift
+++ UtilsKit/Helpers/String/String+ExtractValueFromRegex.swift
@@ -27,14 +27,25 @@ extension String {
      - returns: all string who respect the regex
      */
     public func values(forRegex regex: String) -> [String] {
-        do {
-            let regex = try NSRegularExpression(pattern: regex, options: [])
-            let nsString = NSString(string: self)
-            let results: [NSTextCheckingResult] = regex.matches(in: self, options: [], range: NSRange(location: 0, length: nsString.length))
-            return results.map { nsString.substring(with: $0.range(at: 1)) }
-        } catch {
-            log(.data, "Regex \(regex) in \(self)", error: error)
-            return []
-        }
+		do {
+			let regex = try NSRegularExpression(pattern: regex, options: [])
+			let matches = regex.matches(in: self,
+										options: [],
+										range: NSRange(location: 0,
+													   length: self.utf16.count))
+			guard let match = matches.first else { return [] }
+			var array: [String] = []
+			
+			for index in 1...match.numberOfRanges - 1 {
+				let range = match.range(at: index)
+				if let swiftRange = Range(range, in: self) {
+					array.append(String(self[swiftRange]))
+				}
+			}
+			return array
+		} catch {
+			log(.data, "Regex \(regex) in \(self)", error: error)
+			return []
+		}
     }
 }
