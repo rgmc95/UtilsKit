ed65a8d358389cf5305a0b4a458e173f18257f3b
Michael Coqueret <mcoqueret@exomind.fr>
2023-06-22 16:53:04 +0200

    [FIX] email regex

--- Sources/UtilsKitHelpers/String/String+Validate.swift
+++ Sources/UtilsKitHelpers/String/String+Validate.swift
@@ -18,7 +18,7 @@ extension String {
      - returns: a boolean indicating if the string in a valid email format.
      */
     public var isValidEmail: Bool {
-        let regex: String = "^.+@.+\\..+$"
+        let regex: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
         return predicate.evaluate(with: self)
     }
