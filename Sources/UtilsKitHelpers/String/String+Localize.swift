b7fd1ba320ae85eeea94396075b676c81a671e6b
Michael Coqueret <mcoqueret@exomind.fr>
2023-07-05 18:38:35 +0200

    [Locale] change current language

--- Sources/UtilsKitHelpers/String/String+Localize.swift
+++ Sources/UtilsKitHelpers/String/String+Localize.swift
@@ -8,21 +8,45 @@
 
 import Foundation
 
+fileprivate var _currentLanguages: [String]?
+
+extension Locale {
+	
+	public static var currentLanguages: [String] {
+		get {
+			_currentLanguages ?? (UserDefaults.standard.array(forKey: "AppleLanguages") as? [String]) ?? []
+		}
+		set {
+			_currentLanguages = newValue
+		}
+	}
+}
+
 extension String {
-    
-    /**
-     Localize the string.
-     
-     - returns: a localized string.
-     */
-    public var localized: String {
-		NSLocalizedString(self, comment: "")
-    }
-    
-    /**
-     Localize the string.
-     
-     - returns: a localized string or nil.
-     */
-    public var localizedOrNil: String? { (localized != self ? localized : nil) }
+	
+	/**
+	 Localize the string.
+	 
+	 - returns: a localized string.
+	 */
+	public var localized: String {
+		for language in Locale.currentLanguages {
+			if let bundlePath = Bundle.main.path(forResource: language, ofType: "lproj"),
+			   let bundle = Bundle(path: bundlePath) {
+				return NSLocalizedString(self,
+										 bundle: bundle,
+										 value: NSLocalizedString(self, comment: ""),
+										 comment: "")
+			}
+		}
+		
+		return NSLocalizedString(self, comment: "")
+	}
+	
+	/**
+	 Localize the string.
+	 
+	 - returns: a localized string or nil.
+	 */
+	public var localizedOrNil: String? { (localized != self ? localized : nil) }
 }
