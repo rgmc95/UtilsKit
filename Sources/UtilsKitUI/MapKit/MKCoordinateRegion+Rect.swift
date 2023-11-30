a88f1cc717179674f2a50145e1ffe1c78ffc12b8
Michael Coqueret <mcoqueret@exomind.fr>
2023-11-30 15:44:17 +0100

    [MapKit] add zoom level

---
+++ Sources/UtilsKitUI/MapKit/MKCoordinateRegion+Rect.swift
@@ -0,0 +1,29 @@
+//
+//  MKCoordinateRegion+Rect.swift
+//
+//
+//  Created by Michael Coqueret on 30/11/2023.
+//
+
+import Foundation
+import MapKit
+
+extension MKCoordinateRegion {
+	
+	/// Get rect from region
+	public func getRect() -> MKMapRect {
+		let topLeft = CLLocationCoordinate2D(latitude: self.center.latitude + (self.span.latitudeDelta / 2),
+											 longitude: self.center.longitude - (self.span.longitudeDelta / 2))
+		
+		let bottomRight = CLLocationCoordinate2D(latitude: self.center.latitude - (self.span.latitudeDelta / 2),
+												 longitude: self.center.longitude + (self.span.longitudeDelta / 2))
+		
+		let a = MKMapPoint(topLeft)
+		let b = MKMapPoint(bottomRight)
+		
+		return MKMapRect(origin: MKMapPoint(x: min(a.x, b.x),
+											y: min(a.y, b.y)),
+						 size: MKMapSize(width: abs(a.x - b.x),
+										 height: abs(a.y - b.y)))
+	}
+}
---
+++ Sources/UtilsKitUI/MapKit/MKMapView+ZoomLevel.swift
@@ -0,0 +1,36 @@
+//
+//  MKMapView+ZoomLevel.swift
+//
+//
+//  Created by Michael Coqueret on 30/11/2023.
+//
+
+import Foundation
+import MapKit
+
+extension MKMapView {
+	
+	/// Current zoom level
+	public var zoomLevel: Int {
+		Int(log2(360 * (Double(self.frame.size.width / 256) / self.region.span.longitudeDelta)))
+	}
+	
+	private func getDelta(for zoomLevel: Int) -> MKCoordinateSpan {
+		MKCoordinateSpan(latitudeDelta: 0,
+						 longitudeDelta: 360 / pow(2, Double(zoomLevel)) * Double(self.frame.size.width) / 256)
+	}
+	
+	// Set center in coordinate with zoom and inset
+	public func setCenter(coordinate: CLLocationCoordinate2D,
+				   zoomLevel: Int?,
+				   inset: UIEdgeInsets = .zero,
+				   animated: Bool = true) {
+		let span = zoomLevel == nil ? region.span : self.getDelta(for: zoomLevel ?? 0)
+		let region = MKCoordinateRegion(center: coordinate,
+										span: span)
+		
+		self.setVisibleMapRect(region.getRect(),
+							   edgePadding: inset,
+							   animated: animated)
+	}
+}
