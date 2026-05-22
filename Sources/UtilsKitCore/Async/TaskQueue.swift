8043c9f8e5eca17a2cde53936d6c03e6c1248b45
Michael Coqueret <coqueretmichael@gmail.com>
2026-05-22 16:14:11 +0200

    [TaskQueue] cancel task

--- Sources/UtilsKitCore/Async/TaskQueue.swift
+++ Sources/UtilsKitCore/Async/TaskQueue.swift
@@ -7,17 +7,22 @@
 
 public actor TaskQueue {
 	
-	private var task: Task<Void, Error>?
+	private var task: Task<Void, any Error>?
 	
 	public init() { }
 	
-	// Wait for the current task to complete, if any
+	/// Cancel task
+	public func cancel() {
+		self.task?.cancel()
+	}
+	
+	/// Wait for the current task to complete, if any
 	public func wait() async {
 		guard let task, !task.isCancelled else { return }
 		_ = try? await task.value
 	}
 	
-	// Execute the given block as a new task, if any
+	/// Execute the given block as a new task, if any
 	public func fire(_ block: @Sendable @escaping () async throws -> Void) async throws {
 		if let task, !task.isCancelled {
 			try await task.value
@@ -32,7 +37,7 @@ public actor TaskQueue {
 		}
 	}
 	
-	// Wait for the current task to complete and then execute the given block as a new task
+	/// Wait for the current task to complete and then execute the given block as a new task
 	public func waitAndFire(_ block: @Sendable @escaping () async throws -> Void) async throws {
 		if let task, !task.isCancelled {
 			let currentTask = Task {
